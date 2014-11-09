f312f61ee42ad51ef574add9a0c75333d7f632da
Uneeb Agha <uneeb.i.am@gmail.com>
2014-11-09 18:59:51 +0100

    Added basic visuals fixed db

--- personal/settings.py
+++ personal/settings.py
@@ -66,12 +66,12 @@ DATABASES = {
 }
 
 # FOR LOCAL
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
@@ -103,7 +103,7 @@ STATICFILES_FINDERS = (
 # Parse database configuration from $DATABASE_URL
 
 # FOR HEROKU
-# DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
